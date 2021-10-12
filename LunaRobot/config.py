e676d83a0512a2d45b7f3ce7f569f3c593da2414
RAXSBOT <86977613+Rakaxyxz@users.noreply.github.com>
2021-10-13 01:08:32 +0700

    Update config.py

--- LunaRobot/config.py
+++ LunaRobot/config.py
@@ -16,9 +16,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 797768146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "tdrki_1" 
-    SUPPORT_CHAT = "todorokisupportgrup"  # Your own group for support, do not add the @
+    OWNER_ID = 5778889 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ImThelastKingMs" 
+    SUPPORT_CHAT = "Mimik_susu1"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001392103597
     )  # Prints any new group the bot is added to, prints just the name and ID.
